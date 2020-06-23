93d94d3df9d5b4529124bb6f8342ae5558a1240e
ajhdrummer <thedrew88@gmail.com>
2020-06-23 12:07:23 -0400

    added collision detection for projectiles. cleaned up code

--- index4.js
+++ index4.js
@@ -3,26 +3,92 @@ canvas.height = window.innerHeight-70
 canvas.width = window.innerWidth-70
 let ctx = canvas.getContext('2d')
 
-const controls = {
-		left:false,
-		right:false,
-		up:false,
+class Game{
+    constructor(level,player,boss){
+        this.level = level
+        this.player = player
+        this.boss= boss
+        this.controls = {
+            left:false,
+            right:false,
+            up:false
+    }
 
-		keyListener: function(event){
-			let keyState = (event.type == "keydown")? true:false;
+    this.keyListener = (event) => {
+        let keyState = (event.type == "keydown")? true:false;
+        switch(event.key){
+            case 'ArrowLeft':
+            this.controls.left = keyState
+            break
+            case 'ArrowRight':
+            this.controls.right = keyState
+            break
+            case ' ':
+            this.controls.up = keyState
+        }
+    }
 
-			switch(event.key){
-				case 'ArrowLeft':
-				controls.left = keyState
-				break
-				case 'ArrowRight':
-				controls.right = keyState
-				break
-				case ' ':
-				controls.up = keyState
-			}
-		}
-	}
+}
+
+    playerBossCollision = (rect1, rect2) =>{
+
+        if(rect1.grounded==false &&
+           (!(rect1.y +rect1.height > rect2.y+15)) &&      
+           rect1.x < rect2.x + rect2.width &&
+           rect1.x + rect1.width > rect2.x &&
+           rect1.y < rect2.y + rect2.height &&
+           rect1.y + rect1.height > rect2.y){
+            if(rect1.cooldown ==0){
+                rect1.y=rect2.y-rect1.height
+            if(rect2.cooldown==0){
+                rect2.health-=1
+                rect2.cooldown=20
+                }
+            }
+        }
+
+        if(rect1.x < rect2.x + rect2.width &&
+           rect1.x + rect1.width > rect2.x &&
+           rect1.y < rect2.y + rect2.height &&
+           rect1.y + rect1.height > rect2.y){
+            if(rect1.cooldown==0){
+                rect1.health-=1
+                rect1.cooldown=20
+            }
+        }
+
+   }
+
+    projectileCollisionDetection = (rect1, rect2) => {
+    if(rect1.x < rect2.x + rect2.width &&
+       rect1.x + rect1.width > rect2.x &&
+       rect1.y < rect2.y + rect2.height &&
+       rect1.y + rect1.height > rect2.y){
+        if(rect1.cooldown==0){
+                rect1.health-=1
+                rect1.cooldown=20
+            }
+    }
+    }
+
+    floorDetection = (rect1, rect2) => {
+    return (rect1.y + rect1.height >= rect2.y)
+    }
+
+    platformDetection = (rect1, rect2) => {
+    return (rect1.x < rect2.x + rect2.width &&
+       rect1.x + rect1.width > rect2.x &&
+       rect1.y < rect2.y + rect2.height &&
+       rect1.y + rect1.height > rect2.y)
+    }
+
+    hitPlayerHead = (rect1, rect2) =>{
+   return (rect1.x < rect2.x + rect2.width &&
+       rect1.x + rect1.width > rect2.x &&
+       rect1.y<rect2.y+rect2.height&&
+       rect1.y+rect1.height>rect2.y+(rect1.height/2))
+    }
+}
 
 class Projectile {
     constructor(x,y){
@@ -50,8 +116,10 @@ class Boss {
     this.movingLeft=false
     this.movingRight=false
     this.cooldown=0
-    this.projectiles=null
-    this.projectileFired=false
+    this.projectileLeft=null
+    this.projectileFiredLeft=false
+    this.projectileRight=null
+    this.projectileFiredRight=false
     }
 
     drawBoss = (x,y) => {
@@ -66,120 +134,64 @@ class Boss {
 
     }
 
-    /*attack = () =>{
-
-        if (this.phase==1){
-            this.attack1()
-        }
-
-        if(this.phase==2){
-            this.attack1()
-            this.attack2()
-        }
-    }*/
-
     attack1 = () => {
 
         if(this.x >= canvas.width-this.width){
-                this.movingRight=false
-                this.movingLeft=true
-                this.x=canvas.width-this.width
-                this.xVelocity=0
+            this.movingRight=false
+            this.movingLeft=true
+            this.x=canvas.width-this.width
+            this.xVelocity=0
             }
 
-            if(this.movingLeft==true){    
-                this.xVelocity-=0.5
-                this.x += this.xVelocity;
-/*              this.xVelocity*=.9
-*/            
+        if(this.movingLeft==true){    
+            this.xVelocity-=0.5
+            this.x += this.xVelocity;
             }
 
-            if(this.x <= 0){
-                this.movingLeft=false
-                this.movingRight=true
-                this.x=1
-                this.xVelocity=0
+        if(this.x <= 0){
+            this.movingLeft=false
+            this.movingRight=true
+            this.x=1
+            this.xVelocity=0
             }
 
-            if(this.movingRight==true){
-                this.xVelocity+=0.5
-                this.x += this.xVelocity;
-/*              this.xVelocity*=.9
-*/            }
-
+        if(this.movingRight==true){
+            this.xVelocity+=0.5
+            this.x += this.xVelocity;           
+            }
     }
 
     attack2 = () =>{
-            if(this.projectileFired==false){
-            this.projectileFired=true    
-            this.projectiles = new Projectile(this.x,this.y+(this.height/2))}
+        if(this.projectileFiredLeft==false && this.movingLeft==true){
+            this.projectileFiredLeft=true    
+            this.projectileLeft = new Projectile(this.x,this.y+(this.height/2))
+            this.projectileLeft.xVelocity=this.xVelocity}
         
+        if(this.projectileLeft!=null){    
+            this.projectileLeft.xVelocity -=1.5
+            ctx.fillStyle=this.projectileLeft.color
+            ctx.fillRect(this.projectileLeft.x+=this.projectileLeft.xVelocity, this.projectileLeft.y, this.projectileLeft.width, this.projectileLeft.height)
+
+        if(this.projectileLeft.x<0){
+            this.projectileFiredLeft=false
+            }
+        }
+
+        if(this.projectileFiredRight==false && this.movingRight==true){
+            this.projectileFiredRight=true    
+            this.projectileRight = new Projectile(this.x,this.y+(this.height/2))
+            this.projectileRight.xVelocity=this.xVelocity}
             
-       
-        ctx.fillStyle=this.projectiles.color
-        ctx.fillRect(this.projectiles.x-=10, this.projectiles.y, this.projectiles.width, this.projectiles.height)
+            if(this.projectileRight!=null){    
+                this.projectileRight.xVelocity +=1.5
+                ctx.fillStyle=this.projectileRight.color
+                ctx.fillRect(this.projectileRight.x+=this.projectileRight.xVelocity, this.projectileRight.y, this.projectileRight.width, this.projectileRight.height)
 
-        if(this.projectiles.x<0){
-            this.projectileFired=false
-        }
-    
-}
-}
-
-
-
-class Game{
-	constructor(level,player,boss){
-	this.level = level
-	this.player = player
-    this.boss= boss
-
-	}
-
-    playerBossCollision = (rect1, rect2) =>{
-
-        if(rect1.grounded==false &&     
-       rect1.x < rect2.x + rect2.width &&
-       rect1.x + rect1.width > rect2.x &&
-       rect1.y < rect2.y + rect2.height &&
-       rect1.y + rect1.height > rect2.y){
-            if(rect1.cooldown ==0){
-            rect1.y=rect2.y-rect1.height
-            if(rect2.cooldown==0){
-                rect2.health-=1
-                rect2.cooldown=20
-            }
-        }
-        }
-
-        if(rect1.x < rect2.x + rect2.width &&
-       rect1.x + rect1.width > rect2.x &&
-       rect1.y < rect2.y + rect2.height &&
-       rect1.y + rect1.height > rect2.y){
-            if(rect1.cooldown==0){
-                rect1.health-=1
-                rect1.cooldown=20
+            if(this.projectileRight.x>canvas.width){
+                this.projectileFiredRight=false
             }
         }
 
-   }
-
-    floorDetection = (rect1, rect2) => {
-    return (rect1.y + rect1.height >= rect2.y)
-    }
-
-    platformDetection = (rect1, rect2) => {
-    return (rect1.x < rect2.x + rect2.width &&
-       rect1.x + rect1.width > rect2.x &&
-       rect1.y < rect2.y + rect2.height &&
-       rect1.y + rect1.height > rect2.y)
-    }
-
-    hitPlayerHead = (rect1, rect2) =>{
-   return (rect1.x < rect2.x + rect2.width &&
-   rect1.x + rect1.width > rect2.x &&
-   rect1.y<rect2.y+rect2.height&&
-   rect1.y+rect1.height>rect2.y+(rect1.height/2))
     }
 }
 
@@ -195,7 +207,7 @@ class Level {
         let a =[]
 		for (const property in this.surfaces){
 
-            for(let innerProperty in this.surfaces[property]){
+            for(const innerProperty in this.surfaces[property]){
                 a.push(this.surfaces[property][innerProperty])}
 		
             ctx.fillRect(a[0],a[1],a[2],a[3])
@@ -237,6 +249,39 @@ class Player {
 		ctx.fillRect(x, y, this.width, this.height)
 
 	}
+
+    movePlayer = () => {
+
+        if(g.controls.up && this.jumping==false){
+            this.yVelocity-=35
+            this.jumping=true
+            this.grounded=false
+        }
+
+        if(g.controls.left){
+            this.xVelocity-=0.5
+        }
+
+        if(g.controls.right){
+            this.xVelocity+=0.5
+        }
+
+        this.yVelocity+=1.5;
+        this.x += g.player.xVelocity;
+        this.y+=g.player.yVelocity;
+        this.xVelocity*=0.9
+        this.yVelocity*=0.9
+        this.grounded=false
+
+        if(this.x<0){
+            this.x=0
+        }
+
+        if(this.x>canvas.width-this.width){
+            this.x=canvas.width-this.width
+        }
+
+    }
 }
 
 function animationLoop() {
@@ -246,68 +291,23 @@ function animationLoop() {
     g.player.drawPlayer(g.player.x,g.player.y)
     g.boss.drawBoss(g.boss.x,g.boss.y)
     g.boss.attack1()
-    if(g.boss.phase==2){
-        g.boss.attack2()
-    }
+
     document.getElementById('player-health').innerText=g.player.health
     document.getElementById('boss-health').innerText=g.boss.health
 
-    if(controls.up && g.player.jumping==false){
-    	g.player.yVelocity-=35
-    	g.player.jumping=true
-        g.player.grounded=false
+    if(g.boss.phase==2){
+        g.boss.attack2()
+        if(g.boss.projectileLeft != null)
+            g.projectileCollisionDetection(g.player,g.boss.projectileLeft)
+        if(g.boss.projectileRight != null)
+            g.projectileCollisionDetection(g.player,g.boss.projectileRight)
     }
 
-    if(controls.left){
-    	g.player.xVelocity-=0.5
-    }
-
-    if(controls.right){
-    	g.player.xVelocity+=0.5
-    }
-
-    g.player.yVelocity+=1.5;
-    g.player.x += g.player.xVelocity;
-    g.player.y+=g.player.yVelocity;
-    g.player.xVelocity*=0.9
-    g.player.yVelocity*=0.9
-    g.player.grounded=false
-
-    if(g.player.x<0){
-    	g.player.x=0
-    }
-
-    if(g.player.x>g.level.width-g.player.width){
-    	g.player.x=g.level.width-g.player.width
-    }
-
-    if(g.hitPlayerHead(g.player, g.level.surfaces.plat1)){
-        g.player.y=g.level.surfaces.plat1.y+2
-        g.player.yVelocity=0
-    }
-
-    if(g.hitPlayerHead(g.player, g.level.surfaces.plat2)){
-        g.player.y=g.level.surfaces.plat1.y+2
-        g.player.yVelocity=0
-    }
+    g.player.movePlayer()
 
     if(g.floorDetection(g.player, g.level.surfaces.floor)){
         g.player.jumping=false
         g.player.y=g.level.surfaces.floor.y-g.player.height
-        g.player.yVelocity=0
-        g.player.grounded=true
-    }
-
-    if(g.platformDetection(g.player, g.level.surfaces.plat1)){
-        g.player.jumping=false
-        g.player.y=g.level.surfaces.plat1.y-g.player.height
-        g.player.yVelocity=0
-        g.player.grounded=true
-    }
-
-    if(g.platformDetection(g.player, g.level.surfaces.plat2)){
-        g.player.jumping=false
-        g.player.y=g.level.surfaces.plat1.y-g.player.height
         g.player.yVelocity=0
         g.player.grounded=true
     }
@@ -325,23 +325,9 @@ function animationLoop() {
     if(g.boss.health==40){
         g.boss.phase=2
     }
-
-    
-/*    g.boss.drawProjectile(g.boss.projectiles)
-*/    
 }
 
-let level1Surfaces = {
-
-	floor: {x:0, y:canvas.height-1, width:canvas.width,heigh:2},
-
-	plat1: {x:canvas.width/7, y:canvas.height-150, width:200, height:2},
-
-	plat2: {x:canvas.width*.75, y:canvas.height-150, width:200, height:2}
-}
-
-
-let l = new Level(level1Surfaces)
+let l = new Level({floor: {x:0, y:canvas.height-1, width:canvas.width,heigh:2}})
 let p = new Player()
 let b = new Boss()
 let g = new Game(l,p,b)
@@ -349,6 +335,6 @@ g.boss.y=g.level.height-g.boss.height
 g.boss.x=g.level.width-g.boss.width
 g.player.x=0
 g.player.y=g.level.height-g.player.height
-window.addEventListener('keydown', controls.keyListener)
-window.addEventListener('keyup', controls.keyListener)
+window.addEventListener('keydown', g.keyListener)
+window.addEventListener('keyup', g.keyListener)
 animationLoop()
